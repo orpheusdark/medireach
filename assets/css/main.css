a6b74eabdd3f242778889b1fce30cb61b4d99ec7
orpheusdark <117748842+orpheusdark@users.noreply.github.com>
2024-12-16 08:19:51 +0530

    changed main

--- assets/css/main.css
+++ assets/css/main.css
@@ -1,6 +1,6 @@
 /**
-* Template Name: Medilab
-* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+* Template Name: Carepulse
+* Template URL: https://bootstrapmade.com/Carepulse-free-medical-bootstrap-theme/
 * Updated: Aug 07 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
